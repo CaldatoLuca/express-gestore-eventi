fa440dfc274805c490c001c5b35f722ee74be94b
CaldatoLuca <lucacaldato15@gmail.com>
2024-05-31 11:37:03 +0200

    feat:  milestone 4

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,16 +1,46 @@
+const Event = require("../models/eventModel");
+const { getPath } = require("../utils");
+const events = require("../db/eventsDb.json");
+
 const index = (req, res) => {
+  const filePath = getPath("eventsDb", { extension: "json", directory: "db" });
+  const events = Event.read(filePath);
+
+  if (req.query.id) {
+    const event = events.find((event) => event.id === parseInt(req.query.id));
+    return res.json({
+      message: `Event with id ${req.query.id} found`,
+      status: 200,
+      route: "/events",
+      event,
+    });
+  }
+
   res.json({
-    message: "Index Page",
+    message: "Events List",
     status: 200,
     route: "/events",
+    events,
   });
 };
 
 const store = (req, res) => {
+  const eventToSave = new Event(
+    6,
+    "Evento 6",
+    "Descrizione evento 6",
+    "2024-06-01",
+    100
+  );
+  const filePath = getPath("eventsDb", { extension: "json", directory: "db" });
+  Event.save(filePath, [...events, eventToSave]);
+  events.push(eventToSave);
+
   res.json({
     message: "Store APi",
     status: 200,
     route: "/events ",
+    events,
   });
 };
 
