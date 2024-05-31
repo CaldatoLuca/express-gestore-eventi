ab9faa84e26cecdc8c22dae47c89606e78d4846f
CaldatoLuca <lucacaldato15@gmail.com>
2024-05-31 10:32:16 +0200

    feat: static method read

--- models/eventModel.js
+++ models/eventModel.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+
 class Event {
   constructor(id, title, description, date, maxSeats) {
     this.id = id;
@@ -6,4 +8,20 @@ class Event {
     this.date = new Date(date);
     this.maxSeats = maxSeats;
   }
+
+  /**
+   * given a file path it retun an array of istances of the Event class
+   * the given file must be a json with the correct properties
+   * @param {string} filePath
+   * @returns
+   */
+  static read(filePath) {
+    const jsonFile = fs.readFileSync(filePath, "utf8");
+    const eventsArray = JSON.parse(jsonFile);
+    return eventsArray.map(
+      (e) => new Event(e.id, e.title, e.description, e.date, e.maxSeats)
+    );
+  }
 }
+
+module.exports = Event;
